7e0586c221f1f48f5c48c80c32982a77b76f3a83
Kealoha1988 <kealohaabe1988@gmail.com>
2022-01-28 15:40:57 -0500

    created class Node && class SinglyLinkedList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Data Sturctures!</h1>
+      <h1 style={{color: 'purple'}}>Data Sturctures!</h1>
       <br></br>
-      <h3>testing!!!!</h3>
+      <h3 style={{color: 'blue'}}>testing!!!!</h3>
     </div>
   );
 }
---
+++ src/singlyLInkedList/SLLLogic.js
@@ -0,0 +1,10 @@
+import {Node} from "../GlobalVariables"
+
+export class SinglyLinkedList{
+  constructor(){
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+}
+
